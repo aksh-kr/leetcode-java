d26306281bdec791ec397f96611a8d5f4cd7ac81
Akash Kumar <53661628+aksh-kr@users.noreply.github.com>
2023-06-18 09:49:13 +0530

    Time: 55 ms (51.32%), Space: 56.8 MB (74.34%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,58 +1,33 @@
 class Solution {
-    public int longestConsecutive(int[] arr) {
-		HashMap<Integer, Boolean> map = new HashMap<>();
-		
-		//O(n)
-		for(int i = 0; i < arr.length; i++) {
-			
-			int nm1 = arr[i] - 1;
-			if(map.containsKey(nm1)) {
-				
-				map.put(arr[i], false);
-			}else {
-				map.put(arr[i], true);
-			}
-			
-			int np1 = arr[i] + 1;
-			
-			if(map.containsKey(np1)) {
-				map.put(np1, false);
-			}
-		}
-		
-		ArrayList<Integer> keys = new ArrayList<>(map.keySet());
-		
-		int maxLength = 0;
-		int maxStartingNo = 0;
-		
-		//O(n)
-		for(int key: keys) {
-			
-			if(map.get(key) == true) {
-				
-				int pstartingNo = key;
-				int count = 0;
-				
-				while(map.containsKey(pstartingNo + count)) {
-					count++;
-				}
-				
-				if(count > maxLength) {
-					maxLength = count;
-					maxStartingNo = pstartingNo;
-				}
-			}
-		}
-		
-		ArrayList<Integer> res = new ArrayList<>();
-		
-		//O(n)
-		for(int i = 0; i < maxLength; i++) {
-			res.add(maxStartingNo + i);
-		}
-		
-		return maxLength;
-		
-	}
-
+    public int longestConsecutive(int[] nums) {
+        HashMap<Integer, Boolean> map = new HashMap<>();
+        for(int i : nums) {
+            if(map.containsKey(i-1)){
+                map.put(i, false);
+            }else{
+                map.put(i, true);
+            }
+            if(map.containsKey(i+1)){
+                map.put(i+1, false);
+            }
+        }
+        
+        int maxSequenceLength = 0;
+        
+        ArrayList<Integer> keys = new ArrayList<>(map.keySet());
+        for(int key : keys) {
+            if(map.get(key) == true) {
+                
+                int count = 0;
+                while(map.containsKey(key + count)) {
+                    count++;
+                }
+                
+                if(count > maxSequenceLength) {
+                    maxSequenceLength = count;
+                }
+            }
+        }
+        return maxSequenceLength;
+    }
 }
