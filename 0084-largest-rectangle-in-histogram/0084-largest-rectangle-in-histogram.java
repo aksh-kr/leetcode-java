39ebe7369d0dcff0f515cef5d5692346e47fc599
Akash Kumar <53661628+aksh-kr@users.noreply.github.com>
2023-10-12 00:15:59 +0530

    Time: 68 ms (47.63%), Space: 59.2 MB (21.96%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,68 +1,23 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        
-        int n = heights.length;
-        ArrayList<Integer> right = new ArrayList<>();
-        ArrayList<Integer> left = new ArrayList<>();
-        Stack<Pair<Integer, Integer>> s = new Stack<>();
-        Stack<Pair<Integer, Integer>> s2 = new Stack<>();
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
 
-        int[] width = new int[n];
-        int[] area = new int[n];
-        
-        for(int i=n-1; i>=0; i--) {
-            if(s.size() == 0) {
-                right.add(n);
-            }else if(s.size()>0 && s.peek().getKey() < heights[i]) {
-                right.add(s.peek().getValue());
-            }else if(s.size()>0 && s.peek().getKey() >= heights[i]) {
-                while(s.size()>0 && s.peek().getKey() >= heights[i]) {
-                    s.pop();
-                }
-                
-                if(s.size()==0){
-                    right.add(n);
-                }else{
-                    right.add(s.peek().getValue());
-                }
+        stack.push(-1);
+        for(int i=0; i<heights.length; i++) {
+            while(stack.peek() != -1 && heights[i] <= heights[stack.peek()]) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight*currentWidth);
             }
-            s.push(new Pair<>(heights[i],i));
+            stack.push(i);
         }
-        
-        Collections.reverse(right);
-        
-        for(int i=0; i<n; i++) {
-            if(s2.size() == 0){
-                left.add(-1);
-            } else if(s2.size() > 0 && s2.peek().getKey() < heights[i]) {
-                left.add(s2.peek().getValue());
-            }else if(s2.size() > 0 && s2.peek().getKey() >= heights[i]) {
-                while(s2.size() > 0 && s2.peek().getKey() >= heights[i]) {
-                    s2.pop();
-                }
-                if(s2.size() == 0) {
-                    left.add(-1);
-                }else{
-                    left.add(s2.peek().getValue());
-                }
-            }
-            s2.push(new Pair(heights[i], i));
+
+        while(stack.peek() != -1) {
+             int currentHeight = heights[stack.pop()];
+             int currentWidth = heights.length - stack.peek() - 1;
+             maxArea = Math.max(maxArea, currentHeight*currentWidth);
         }
-        
-        for(int i=0; i<n; i++) {
-            width[i] = right.get(i) - left.get(i) - 1;
-        }
-        
-        for(int i=0; i<n; i++) {
-            area[i] = heights[i] * width[i];
-        }
-        
-        int max = 0;
-        for(int i=0; i<n; i++) {
-            if(area[i]>max){
-                max = area[i];
-            }
-        }
-        return max;
+        return maxArea;
     }
 }
