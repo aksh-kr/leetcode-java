4b68deb2b526f2b70367f790440575468d628313
Akash Kumar <53661628+aksh-kr@users.noreply.github.com>
2024-01-21 21:15:09 +0530

    Time: 2 ms (74.00%), Space: 44.9 MB (54.68%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int stepCount = 0;
+        int lastJumpMax = 0;
+        int currentJumpMax = 0;
+        
+        for(int i=0; i<nums.length-1; i++) {
+            currentJumpMax = Math.max(currentJumpMax, i+nums[i]);
+            if(i == lastJumpMax) {
+                stepCount++;
+                lastJumpMax = currentJumpMax;
+            }
+        }
+        return stepCount;
+    }
+}
